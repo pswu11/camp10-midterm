a389899b40809b035a8033a795b7d7749842e0e7
pswu11 <pswu@protonmail.com>
2023-06-29 10:54:31 +0200

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { HiHome, HiFilm, HiBookmark, HiUser} from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+
+const navItems = 
+
+export function Navbar() {
+  return (
+    <div className="w-full h-[5.5rem] bg-dark-red-100">
+    </div>
+  )
+}
