2ce9f9b53f71fd8f6da10b597cb764687f034c69
stread <irgendwasmitmedien1@gmail.com>
2023-06-29 16:18:01 +0200

    add storybook

---
+++ src/components/GenreIcon.story.tsx
@@ -0,0 +1,17 @@
+import { GenreIcon } from "./GenreIcon";
+
+export default {
+  component: GenreIcon,
+  title: 'GenreIcon',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    icon: {
+      id: '1',
+      title: 'Test GenreIcon',
+      state: 'GenreIcon_default',
+    },
+  },
+};
