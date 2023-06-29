dd6cdda56595207a3f4e77080ff54af2a5055520
radicalgraphical <jacob.wesch@gmail.com>
2023-06-29 11:17:33 +0200

    Setup Tailwind config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <div className=" p-4">
+        {' '}
+        ein div
+        <p className="text-h2 font-700 "> h1 header</p>
+        <p> p test </p>
+      </div>
       <h1 className="text-3xl font-bold underline">Welcome to the Midterm</h1>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'primary-black': '#1C1C27',
+      'secondary-grey': '#363740',
+      'white-standard': '#FFFFFF',
+      'white-dimmed': '#FFFFFF66',
+      'white-dimmed-heavy': '#FFFFFF33',
+      'status-yellow': '#FFB43A',
+      'status-red': '#EF4444',
+      'status-green': '#22C55E',
+    },
+    fontFamily: {
+      sans: ['"Inter"', 'sans-serif'],
+    },
+    fontSize: {
+      l: '1rem',
+      m: '0.875rem',
+      s: '0.750rem',
+    },
+    fontWeight: {
+      700: '700',
+      500: '500',
+    },
+    borderRadius: {
+      24: '24.rem',
+      12: '12.rem',
+      8: '8.rem',
+
+    spacing: {
+      4: '1.25rem',
+      5: '1.5rem',
+      6: '1.75rem',
+      7: '2rem',
+    },
+
     extend: {},
   },
   plugins: [],
