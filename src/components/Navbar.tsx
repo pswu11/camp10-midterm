588f4430c5173e57f9f76167ab282f9d1e3e4349
pswu11 <pswu@protonmail.com>
2023-06-29 12:31:14 +0200

    basic navbar with icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,35 @@
-import { HiHome, HiFilm, HiBookmark, HiUser} from 'react-icons/hi'
-import { Link } from 'react-router-dom'
+import { HiHome, HiFilm, HiBookmark, HiUser } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import { cloneElement } from 'react';
 
-const navItems = 
+const navItems = [
+  {
+    link: '/',
+    icon: <HiHome />,
+  },
+  {
+    link: '/movies',
+    icon: <HiFilm />,
+  },
+  {
+    link: '/bookmarks',
+    icon: <HiBookmark />,
+  },
+  {
+    link: '',
+    icon: <HiUser />,
+  },
+];
 
 export function Navbar() {
   return (
-    <div className="w-full h-[5.5rem] bg-dark-red-100">
+    <div className="">
+      <HiUser className="" />
+      {navItems.map(item => (
+        <Link to={item.link}>
+          {navItems.map(item => cloneElement(item.icon, { className: true }))}
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
