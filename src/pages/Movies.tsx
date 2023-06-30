21d4615bfffd937f36d4a252accbff71c65209b4
JanHendryk <j.h.muenchow@posteo.de>
2023-06-30 15:18:21 +0200

    first types

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,3 +1,17 @@
+
 export function Movies() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTgwNjYxYTU1OWNjYmQyMTVlOWVmNDY1MDQ1YTk0NSIsInN1YiI6IjY0OWVjMmNmYzA3MmEyMDBlY2U0ZTZmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6phbcGXv_1M10j_rCSXwoLwFf5oRb8i8wbJbC2ob_mc'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
   return <div>Movies placeholder.</div>;
 }
+
---
+++ src/types/api.ts
@@ -0,0 +1,45 @@
+export type UpcomingMovie = {
+    
+    adult: Boolean
+    backdrop_path: String
+    genre_ids: [Number]
+    id: Number
+    original_language: String
+    original_title: String
+    overview: String
+    poster_path: String
+    release_date: String
+    title: String
+    video: Boolean
+    vote_average: Number
+    vote_count: Number
+
+
+}
+
+const movieResponse: UpcomingMovie[] = []
+
+
+export type Movie = {
+
+}
+
+export type MovieDetail = {
+
+}
+
+export type Person = {
+
+}
+
+export type Cast = {
+
+}
+
+export type Crew = {
+
+}
+
+export type Credits = {
+
+}
