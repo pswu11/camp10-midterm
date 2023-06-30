4ab51967088f9727014b1229d9758eb6a63d712d
hottinho <christian.erll@gmail.com>
2023-06-30 15:18:38 +0200

    deleted stub in home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,3 @@
-import { Input } from '../components/Input';
-
 export function Home() {
-  return (
-    <div>
-      This is home page
-      <Input variant="login" id="login" label="login" />
-      <Input variant="search" id="search" label="search" />
-    </div>
-  );
+  return <div>This is home page</div>;
 }
