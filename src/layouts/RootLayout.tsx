5c60c4b06415daa2e06c07dbc7d4c05073d14357
pswu11 <pswu@protonmail.com>
2023-06-30 10:52:21 +0200

    tidy up

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from '../components/Navbar';
 export function RootLayout() {
   return (
     <div className='bg-dark text-white flex flex-col justify-between m-auto w-[375px] h-[667px] rounded-3xl'>
-      <div>
+      <div className='m-auto'>
         <Outlet />
       </div>
       <Navbar />
