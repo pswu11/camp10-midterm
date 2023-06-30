faab367879f9d6b76c8cc4cebdd7ad44ac98b42e
hottinho <christian.erll@gmail.com>
2023-06-30 15:40:00 +0200

    "styling when focussed"

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,15 +9,15 @@ interface InputProps extends Input {
 export function Input({ icon, placeholder, id, ...props }: InputProps) {
   return (
     <label
-      htmlFor={props.id}
-      className="flex bg-dark-light px-4 py-2 rounded text-m font-500 outline-none focus:outline-white-dimmed"
+      htmlFor={id}
+      className="flex bg-dark-light px-4 py-2 rounded-md text-m font-500 outline-none focus:ring-white-dimmed"
     >
       <span className="text-white-dimmed">{icon}</span>
       <input
         id={id}
         name={id}
         placeholder={placeholder}
-        className="bg-dark-light"
+        className="bg-dark-light outline-none"
         {...props}
       />
     </label>
