424c8d90c28711cd1390e42d4c9e4cb6cd548f96
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-30 11:27:15 +0200

    resolved key warning in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,6 +26,7 @@ export function Navbar() {
     <div className="flex gap-12 justify-center items-center h-[5.5rem] w-full">
       {navItems.map(item => (
         <NavItem
+          key={item.link}
           link={item.link}
           icon={cloneElement(item.icon)}
         />
