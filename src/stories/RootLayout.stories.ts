317890b5e7f52368551911497ad0b4ae295b96f5
pswu11 <pswu@protonmail.com>
2023-06-30 10:15:53 +0200

    add react router addon

--- src/stories/RootLayout.stories.ts
+++ src/stories/RootLayout.stories.ts
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { RootLayout } from '../layouts/RootLayout';
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta: Meta<typeof RootLayout> = {
   component: RootLayout,
+  decorators: [withRouter],
 };
 
 export default meta;
