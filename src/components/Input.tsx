f9b374386015857c8bbc47a192e982c76b213cab
hottinho <christian.erll@gmail.com>
2023-06-30 16:35:16 +0200

    finished input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 type Input = React.InputHTMLAttributes<HTMLInputElement>;
 
 interface InputProps extends Input {
-  icon?: React.ReactNode;
+  icon: JSX.Element;
 }
 
 export function Input({ icon, placeholder, id, ...props }: InputProps) {
   return (
     <label
       htmlFor={id}
-      className="flex bg-dark-light px-4 py-2 rounded-md text-m font-500 outline-none focus:ring-white-dimmed"
+      className="flex items-center  bg-dark-light px-5 py-3 gap-5 rounded-md outline-none focus-within:outline-white-dimmed focus-within:outline-offset-0"
     >
-      <span className="text-white-dimmed">{icon}</span>
+      {cloneElement(icon, { className: 'w-6 h-6 text-white-dimmed' })}
       <input
         id={id}
         name={id}
         placeholder={placeholder}
-        className="bg-dark-light outline-none"
+        className="bg-dark-light text-white w-full text-m font-500 flex-1 placeholder-white-dimmed outline-none"
         {...props}
       />
     </label>
