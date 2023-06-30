5e834342432b2f31b4d444206aaa72244647c561
hottinho <christian.erll@gmail.com>
2023-06-30 14:48:10 +0200

    "styling trick - label is container"

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,34 +1,27 @@
 import React from 'react';
-import { cn } from '../lib/utils';
 
 type Input = React.InputHTMLAttributes<HTMLInputElement>;
 
 interface InputProps extends Input {
-  label: string;
-  id: string;
-  variant?: 'login' | 'search';
+  icon?: JSX.Element;
 }
 
-export function Input({
-  variant = 'login',
-  className,
-  id,
-  ...props
-}: InputProps) {
-  const styles: Record<typeof variant, string> = {
-    login: 'rounded bg-dark-light text-white',
-    search: 'rounded-full bg-red',
-  };
-
+export function Input({ icon, ...props }: InputProps) {
   return (
-    <label htmlFor={id} className="border-dark-light">
-      <span className="text-white">{label}:asdasd</span>
-      <input
-        className={cn('flex items-center', styles[variant], className)}
-        id={id}
-        name={id}
-        {...props}
-      />
-    </label>
+    <>
+      <label
+        htmlFor={props.id}
+        className="flex bg-dark-light px-2 py-1 rounded text-s outline-none focus:outline-white-dimmed"
+      >
+        <span className="text-white-dimmed align-middle">{icon}</span>
+        <input
+          id={props.id}
+          name={props.id}
+          placeholder={props.placeholder}
+          className="bg-dark-light"
+          {...props}
+        />
+      </label>
+    </>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+import { Input } from '../components/Input';
+import { HiLogin } from 'react-icons/hi';
+
 export function Login() {
-  return <p>Hello, please log in</p>;
+  const [userData, setUserData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const { username, password } = userData;
+
+  const changeHandler = e => {
+    setUserData({ ...userData, [e.target.name]: [e.target.value] });
+  };
+
+  return (
+    <form className="flex flex-col items-center justify-center gap-2 bg-dark">
+      <Input
+        placeholder="your@email.com"
+        value={username}
+        onChange={changeHandler}
+      />
+      <Input
+        placeholder="a"
+        icon={<HiLogin />}
+        value={password}
+        onChange={changeHandler}
+      />
+    </form>
+  );
 }
