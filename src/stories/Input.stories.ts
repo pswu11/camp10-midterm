9f4f5f9623a739771839e7ce2f14c1b1cda9a029
hottinho <christian.erll@gmail.com>
2023-07-03 12:30:22 +0200

    add storybook

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Input } from '../components/Input';
+import { HiKey } from 'react-icons/hi';
 
 const meta: Meta<typeof Input> = {
   component: Input,
@@ -9,4 +10,16 @@ export default meta;
 
 type Story = StoryObj<typeof Input>;
 
-export const Login: Story = {};
+export const Login: Story = {
+  args: {
+    id: 'login',
+    placeholder: 'your@email.com',
+  },
+};
+
+export const Password: Story = {
+  args: {
+    id: 'password',
+    placeholder: 'Enter password',
+  },
+};
