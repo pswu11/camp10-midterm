d0f832b65a0a15f0a318acf58bd13b4c89c79ed5
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-03 13:17:14 +0200

    fixed some styling and added a feature, if no image then create user badge

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,22 +1,32 @@
 import Avatar from '../assets/Avatar.png';
 
 export type Props = {
-    name?: string;
-    image?: string;
+  name?: string;
+  image?: string;
 };
 
-function UserInfo({ name = 'Human Person', image = Avatar, ...props }: Props) {
-    return (
-        <header className="flex justify-between bg-dark">
-            <div className="flex flex-col justify-between">
-                <h3 className="text-white text-s font-700 opacity-40">Welcome {name} 👋</h3>
-                <h1 className="text-white text-m font-700">Let's relax and watch a movie!</h1>
-            </div>
-            <div>
-                <img className="h-10 w-10" src={image}></img>
-            </div>
-        </header>
-    );
+function UserInfo({ name = 'Human Person', image = Avatar }: Props) {
+  const initial = name.charAt(0).toUpperCase();
+
+  return (
+    <header className="flex justify-between">
+      <div className="flex flex-col justify-between gap-3">
+        <h3 className="text-white-dimmed text-s font-700">
+          Welcome {name} <span className="text-white">👋</span>
+        </h3>
+        <h1 className="text-white text-m font-700">
+          Let's relax and watch a movie!
+        </h1>
+      </div>
+      <div className="h-10 w-10 rounded-full bg-dark-light flex items-center justify-center text-white">
+        {image ? (
+          <img className="h-full w-full rounded-full" src={image} alt={name} />
+        ) : (
+          initial
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default UserInfo;
