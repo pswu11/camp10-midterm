26e42d35a0863c6c4e88a1fe84a46f11951ad901
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-03 12:34:26 +0200

    added styling for disabled button, fixed roundedness, fixed text fonts and added labels

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,35 +6,47 @@ type ButtonSize = 'default' | 'small';
 type ButtonProps = {
   variant?: ButtonVariant;
   size?: ButtonSize;
+  label?: string;
 } & React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
+const variantStyles: Record<ButtonVariant, string> = {
+  primary: 'bg-yellow text-dark-light',
+  secondary: 'bg-dark-light text-white',
+};
+
+const sizeStyles: Record<ButtonSize, string> = {
+  default: 'py-4 text-m',
+  small: 'py-1 text-s',
+};
+
+const disabledStyles = 'opacity-50';
+
 export function Button({
   variant = 'primary',
   size = 'default',
   className,
-  children = 'Button',
+  disabled = false,
+  label = 'Button',
+  children = label,
   ...props
 }: ButtonProps) {
-  const variantStyles: Record<typeof variant, string> = {
-    primary: 'bg-yellow text-dark-light ',
-    secondary: 'bg-dark-light text-white ',
-  };
-  const sizeStyles: Record<typeof size, string> = {
-    default: ' font font-700 py-4 ',
-    small: ' font font-500 py-1 ',
-  };
+  const variantStyle = variantStyles[variant];
+  const sizeStyle = sizeStyles[size];
+  const disabledStyle = disabled ? disabledStyles : '';
 
   return (
     <button
       className={cn(
-        'flex justify-center text-center text-s px-9  w-full rounded',
-        variantStyles[variant],
-        sizeStyles[size],
+        'flex justify-center font-700 text-center px-9 w-full rounded-lg',
+        variantStyle,
+        sizeStyle,
+        disabledStyle,
         className
       )}
+      disabled={disabled}
       {...props}
     >
       {children}
