8a108c1ae6cc6cd9bee6ed3930dfae74c8e176d2
pswu11 <pswu@protonmail.com>
2023-07-04 17:06:23 +0200

    ticket page component

--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -5,14 +5,16 @@ import { TicketInfo } from '../components/TicketInfo';
 type TicketType = {
   id: string
   movieTitle: string
+  backdrop_path: string
   date: string
   time: string
   price: number
   seat: string[]
 }
 
-const currentMovie: TicketType = {
-  id: "13D53987xF",
+const currentTicket: TicketType = {
+  id: "325398732342",
+  backdrop_path: "https://image.tmdb.org/t/p/w500/7eccX0xay9pDj6ZQvU4cu3whw18.jpg",
   movieTitle: "Batman: The Doom That Came to Gotham",
   date: "06 Jun",
   time: "16:40",
@@ -22,20 +24,28 @@ const currentMovie: TicketType = {
 
 export function Ticket() {
   return (
-    <div className="w-full py-8 px-5">
-      <div className='bg-white-dimmed-heavy rounded-3xl overflow-hidden'>
-        <img src='https://image.tmdb.org/t/p/w500/7eccX0xay9pDj6ZQvU4cu3whw18.jpg'/>
-        <h3 className='text-white font-700 text-[23px]'>{currentMovie.movieTitle}</h3>
-        <div className='flex flex-col'>
-          <span className='font-500 text-s text-dark'>Date</span>
-          <span className='font-700 text-m text-white'>{currentMovie.date}</span>
+    <div className="w-full h-full py-8 px-5 flex flex-col gap-y-4">
+      <div className='bg-white-dimmed-heavy rounded-3xl overflow-hidden flex-1 flex flex-col justify-between'>
+        <div>
+          <img src={currentTicket.backdrop_path} className='h-40 w-full'/>
+          <h3 className='text-white font-700 text-[23px] ml-6 mb-3 mt-1'>{currentTicket.movieTitle}</h3>
+          <div className='flex flex-wrap gap-x-16 gap-y-4 ml-6'>
+            <TicketInfo title='Date' info={currentTicket.date}/>
+            <TicketInfo title='Time' info={currentTicket.time}/>
+            <TicketInfo title='Price' info={currentTicket.price.toString()}/>
+            <TicketInfo title='Seat' info={currentTicket.seat.join(", ")}/>
+          </div>
         </div>
-        <TicketInfo title='Date' info={currentMovie.date}/>
-        <TicketInfo title='Time' info={currentMovie.time}/>
-        <TicketInfo title='Seat' info={currentMovie.seat.join(", ")}/>
-
-        <div>dot line is here</div>
-        <Barcode value="359830823094" />
+        <div className='w-full'>
+          <div className='flex flex-col relative h-12 justify-center'>
+            <div className='border border-dashed border-white border-t-2 border-b-0'></div>
+            <div className='bg-dark rounded-full h-12 w-12 absolute -left-6'></div>
+            <div className='bg-dark rounded-full h-12 w-12 absolute -right-6'></div>
+          </div>
+          <div className='w-full flex justify-center mb-3 -mt-3'>
+            <Barcode value={currentTicket.id} height={60} background='#FFFFFF00' lineColor='#FFFFFF'/>
+          </div>
+        </div>
       </div>
       <Button className='mt-auto'>Back to Home</Button>
     </div>
