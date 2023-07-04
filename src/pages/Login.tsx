91cb2725fed7d9a523015ffc54c16162ee908ae5
pswu11 <pswu@protonmail.com>
2023-07-04 10:17:44 +0200

    login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,27 @@
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import { HiUser } from 'react-icons/hi';
+import { HiKey } from 'react-icons/hi';
+
 export function Login() {
-  return <p>Hello, please log in</p>;
+  return (
+    <form className="bg-dark flex flex-col gap-y-8 w-[375px] h-[667px] rounded-3xl px-5 py-8">
+      <div className="space-y-3">
+        <h2 className="text-l font-700 text-white">Welcome to Cine-Scape</h2>
+        <p className="text-white-dimmed text-m">
+          You need to log in to be able to make reservations and add movies to
+          your watchlist.
+        </p>
+      </div>
+      <div className="space-y-4">
+        <Input placeholder="your@email.com" icon={<HiUser />} type="email" />
+        <Input
+          placeholder="Enter Your Password"
+          icon={<HiKey />}
+          type="password"
+        />
+      </div>
+      <Button className="mt-auto" type='submit'/>
+    </form>
+  );
 }
