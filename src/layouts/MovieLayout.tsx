9154f1cf4cd5f077b53abc3eb4b93a776c5d3a47
hottinho <christian.erll@gmail.com>
2023-07-04 10:51:01 +0200

    background for MovieLayout

--- src/layouts/MovieLayout.tsx
+++ src/layouts/MovieLayout.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 export function MovieLayout() {
   return (
-    <div>
-      This is movie layout.
-      <Outlet/>
+    <div className="bg-dark text-white flex flex-col justify-between m-auto w-[375px] h-[667px] rounded-3xl">
+      <div className="m-auto">
+        <Outlet />
+      </div>
     </div>
   );
 }
