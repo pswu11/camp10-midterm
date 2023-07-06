73b80cfda7d46f43bf452d63cbc526acf95a7af5
hottinho <christian.erll@gmail.com>
2023-07-06 09:28:39 +0200

    CastCrew component now typesafe, hopefully

--- src/components/CastCrew.tsx
+++ src/components/CastCrew.tsx
@@ -1,33 +1,28 @@
-import Woman from '../assets/woman1.jpg';
 import { Cast, Crew } from '../types/api.ts';
 
-type CastCrewProps = {
-  name?: string;
-  image?: string;
-  character?: string;
-  job?: string;
-};
+type CastCrewProps = { person: Cast | Crew };
 
-export default function CastCrew({
-  name = 'Angelina Jolie',
-  character = 'Mrs. Smith',
-  profile_path,
-}: Cast) {
+export default function CastCrew({ person }: CastCrewProps) {
+  // const IS_ACTOR = person.hasOwnProperty('character');
+  const isActor = (person: Cast | Crew): person is Cast =>
+    'character' in person ? true : false;
+  const { name: personName, profile_path: image } = person;
+  const role = isActor(person) ? person.character : person.job;
   return (
     <div className="flex flex-row gap-5">
       <div className="h-16 w-16 bg-dark-light">
         <img
           src={
-            profile_path ||
+            image ||
             'https://images.unsplash.com/photo-1607317146126-64b09b69eb4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80'
           }
-          alt={name}
+          alt={personName}
           className="object-cover w-full h-full"
         />
       </div>
       <div className="flex flex-col justify-center">
-        <h2 className=" text-white font-700 text-m">{name}</h2>
-        <h4 className="text-s text-white-dimmed">{character}</h4>
+        <h2 className=" text-white font-700 text-m">{personName}</h2>
+        <h4 className="text-s text-white-dimmed">{role}</h4>
       </div>
     </div>
   );
--- src/pages/CastAndCrew.tsx
+++ src/pages/CastAndCrew.tsx
@@ -1,35 +1,76 @@
 import CastCrew from '../components/CastCrew';
-import { Cast } from '../types/api';
+import { Cast, Credits, Crew } from '../types/api';
 
-const movieCast: Cast[] = [
-  {
-    id: 1,
-    name: 'Matt Damon',
-    character: 'Will hunting',
-    known_for_department: 'Acting',
-    profile_path: '',
-  },
-  {
-    id: 2,
-    name: 'Robin Williams',
-    character: 'Sean MacGuire',
-    known_for_department: 'Acting',
-    profile_path: '',
-  },
-];
+const castAndCrew: Credits = {
+  id: 1003579,
+  cast: [
+    {
+      adult: false,
+      gender: 2,
+      id: 206757,
+      known_for_department: 'Acting',
+      name: 'David Giuntoli',
+      original_name: 'David Giuntoli',
+      popularity: 8.322,
+      profile_path: '/xxtB8TfKLMw9Utx5v2b3Z0nu5VL.jpg',
+      cast_id: 13,
+      character: 'Batman / Bruce Wayne (voice)',
+      credit_id: '639152c0b9a0bd008451c286',
+      order: 0,
+    },
+    {
+      adult: false,
+      gender: 2,
+      id: 58791,
+      known_for_department: 'Acting',
+      name: 'Patrick Fabian',
+      original_name: 'Patrick Fabian',
+      popularity: 16.663,
+      profile_path: '/ziTKeEraEksMQBg02adD22JJXb0.jpg',
+      cast_id: 2,
+      character: 'Harvey Dent / Two-Face (voice)',
+      credit_id: '639150adb9a0bd008451c148',
+      order: 1,
+    },
+  ],
+  crew: [
+    {
+      adult: false,
+      gender: 2,
+      id: 10949,
+      known_for_department: 'Production',
+      name: 'Michael Uslan',
+      original_name: 'Michael Uslan',
+      popularity: 4.208,
+      profile_path: '/cXiiH0SSk5UHCvHOVAhHX7tNuls.jpg',
+      credit_id: '639155086476540084d5f23c',
+      department: 'Production',
+      job: 'Executive Producer',
+    },
+    {
+      adult: false,
+      gender: 2,
+      id: 66266,
+      known_for_department: 'Writing',
+      name: 'Mike Mignola',
+      original_name: 'Mike Mignola',
+      popularity: 3.633,
+      profile_path: '/cCYHC9bpVv8tfEzQ2dqlEEGOjX0.jpg',
+      credit_id: '62dcad3f2866fa00c5d97ac6',
+      department: 'Writing',
+      job: 'Comic Book',
+    },
+  ],
+};
 
 export function CastAndCrew() {
   return (
-    <div className="flex flex-col gap-5">
-      {movieCast.map(person => (
-        <CastCrew
-          key={person.id}
-          profile_path={person.profile_path}
-          known_for_department={person.known_for_department}
-          id={person.id}
-          name={person.name}
-          character={person.character}
-        />
+    <div className="flex flex-col gap-4">
+      {castAndCrew.cast.map((person: Cast) => (
+        <CastCrew person={person} key={person.id} />
+      ))}
+      {castAndCrew.crew.map((person: Crew) => (
+        <CastCrew key={person.id} person={person} />
       ))}
     </div>
   );
