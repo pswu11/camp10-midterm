6856409b243fef53f6d58f425e359018c0afdf5f
adetuadetu <jposorio95@hotmail.com>
2023-07-07 12:22:35 +0200

    Movieslider, fetched upcoming movies, redirecting to movie details page

--- src/components/Movieslider.tsx
+++ src/components/Movieslider.tsx
@@ -17,10 +17,10 @@ export function Movieslider({upcoming}:UpcomingmovieProps) {
   
     return (
     <>
-        <div className='flex flex-row gap-8 container-snap snap-x snap-mandatory h-screen overflow-scroll w-full'>
+        <div className=' flex flex-row gap-8 container-snap snap-x snap-mandatory h-screen overflow-scroll w-full'>
         {upcoming.map((upcoming) => (
-              <Link className='snap-center w-2/5 -h-[228px] shrink-0 round' to={`/movies/${upcoming.id}`} key={upcoming.id}>
-              <img src={`https://image.tmdb.org/t/p/w500/${upcoming.poster_path}`}/>
+              <Link className=' snap-center w-2/5 -h-[228px] shrink-0' to={`/movies/${upcoming.id}`} key={upcoming.id}>
+              <img className=' hover:scale-110 hover:duration-200 rounded-lg' src={`https://image.tmdb.org/t/p/w500/${upcoming.poster_path}`}/>
               </Link>
               ))}
       
