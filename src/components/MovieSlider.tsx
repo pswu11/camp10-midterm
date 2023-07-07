de050027c5bd0a677b395b804a91e3800e61732b
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-07 12:50:46 +0200

    fixed snap center

--- src/components/MovieSlider.tsx
+++ src/components/MovieSlider.tsx
@@ -5,7 +5,9 @@ export function MovieSlider({ movies }: { movies: UpcomingMovie[] }) {
   return (
     <div className="flex flex-row gap-8 container-snap snap-x snap-mandatory h-screen overflow-scroll w-full">
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} variant="upcoming" />
+        <div key={movie.id} className="snap-center">
+          <MovieCard movie={movie} variant="upcoming" />
+        </div>
       ))}
     </div>
   );
