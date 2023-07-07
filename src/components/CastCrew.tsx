a1383ff6e5ea5426a015d3afbd172fdefc226f23
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-07 10:25:21 +0200

    added correct img source from api

--- src/components/CastCrew.tsx
+++ src/components/CastCrew.tsx
@@ -3,18 +3,18 @@ import { Cast, Crew } from '../types/api.ts';
 type CastCrewProps = { person: Cast | Crew };
 
 export default function CastCrew({ person }: CastCrewProps) {
-  // const IS_ACTOR = person.hasOwnProperty('character');
   const isActor = (person: Cast | Crew): person is Cast =>
-    'character' in person ? true : false;
+    'character' in person;
   const { name: personName, profile_path: image } = person;
   const role = isActor(person) ? person.character : person.job;
+  const imageUrl = image
+    ? `https://image.tmdb.org/t/p/w500${image}`
+    : 'https://images.unsplash.com/photo-1607317146126-64b09b69eb4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80';
   return (
     <div className="flex flex-row gap-5">
       <div className="h-16 w-16 bg-dark-light">
         <img
-          src={
-            'https://images.unsplash.com/photo-1607317146126-64b09b69eb4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80' || image
-          }
+          src={imageUrl}
           alt={personName}
           className="object-cover w-full h-full"
         />
