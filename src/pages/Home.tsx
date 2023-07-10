0516598c7f11e6943dbd0426d639cebaeee3895c
JanHendryk <j.h.muenchow@posteo.de>
2023-07-10 16:43:14 +0200

    homepage tailwind problems still slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,9 @@ import { useLoaderData } from 'react-router-dom';
 import { SearchInput } from '../components/SearchInput';
 import MovieSlider from '../components/MovieSlider';
 import { UpcomingMovie } from '../types/api';
+import UserInfo from '../components/UserInfo';
+import { GenreIcon } from '../components/GenreIcon';
+import { Navbar } from '../components/Navbar';
 
 export function Home() {
   const movies = useLoaderData() as UpcomingMovie[];
@@ -9,8 +12,45 @@ export function Home() {
 
   return (
     <>
-      <SearchInput />
-      <MovieSlider movies={movies} />
+      <section className=" flex flex-col gap-y-6">
+        <UserInfo />
+        <SearchInput />
+        <div>
+          <p className="text-white-dimmed font-500 text-l">Genre</p>
+          <div className="flex flex-row justify-between">
+            <GenreIcon
+              genre={{
+                icon: '',
+                name: '',
+              }}
+            />
+            <GenreIcon
+              genre={{
+                icon: '',
+                name: '',
+              }}
+            />
+            <GenreIcon
+              genre={{
+                icon: '',
+                name: '',
+              }}
+            />
+            <GenreIcon
+              genre={{
+                icon: '',
+                name: '',
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <h3 className="mr-5 text-white text-l font-700 mb-2">
+            Upcoming Movies
+          </h3>
+          <MovieSlider movies={movies} />
+        </div>
+      </section>
     </>
   );
 }
