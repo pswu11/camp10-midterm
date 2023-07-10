863d492aaa01b0f689f409b506eb7627a45df6d8
pswu11 <pswu@protonmail.com>
2023-07-10 17:47:48 +0200

    add basic tabs using headless ui

--- src/pages/CastAndCrew.tsx
+++ src/pages/CastAndCrew.tsx
@@ -1,5 +1,9 @@
 import CastCrew from '../components/CastCrew';
 import { Cast, Credits, Crew } from '../types/api';
+import { Tab } from '@headlessui/react';
+import { MdOutlineKeyboardArrowLeft } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { Button } from '../components/Button';
 
 const castAndCrew: Credits = {
   id: 1003579,
@@ -39,13 +43,35 @@ const castAndCrew: Credits = {
 
 export function CastAndCrew() {
   return (
-    <div className="flex flex-col gap-4">
-      {castAndCrew.cast.map((person: Cast) => (
-        <CastCrew person={person} key={person.id} />
-      ))}
-      {castAndCrew.crew.map((person: Crew) => (
-        <CastCrew key={person.id} person={person} />
-      ))}
+    <div className="h-full w-full px-5 py-8 flex flex-col">
+      <div className="flex relative items-center justify-center">
+        <Link to="/" className="text-white text-xl absolute left-0">
+          <MdOutlineKeyboardArrowLeft />
+        </Link>
+        <h2 className="text-white text-l font-700">Select Date & Time</h2>
+      </div>
+      <Tab.Group>
+        <Tab.List className="flex justify-between mt-8 mb-6">
+          <Tab className="text-white text-s py-1 bg-white-dimmed rounded-md px-16 focus:border outline-none focus:border-white">
+            Cast
+          </Tab>
+          <Tab className="text-white text-s py-1 bg-white-dimmed rounded-md px-16 focus:border outline-none focus:border-white">
+            Crew
+          </Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            {castAndCrew.cast.map((person: Cast) => (
+              <CastCrew person={person} key={person.id} />
+            ))}
+          </Tab.Panel>
+          <Tab.Panel>
+            {castAndCrew.crew.map((person: Crew) => (
+              <CastCrew key={person.id} person={person} />
+            ))}
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
