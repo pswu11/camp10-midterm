28efb22bf1dcc7bbb9aeb76f8e973857076ef71f
pswu11 <pswu@protonmail.com>
2023-07-11 15:30:36 +0200

    remove mock data

--- src/pages/CastAndCrew.tsx
+++ src/pages/CastAndCrew.tsx
@@ -7,42 +7,6 @@ import { Button } from '../components/Button';
 import { Fragment } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-const castAndCrew: Credits = {
-  id: 1003579,
-  cast: [
-    {
-      id: 206757,
-      known_for_department: 'Acting',
-      name: 'David Giuntoli',
-      profile_path: '/xxtB8TfKLMw9Utx5v2b3Z0nu5VL.jpg',
-      character: 'Batman / Bruce Wayne (voice)',
-    },
-    {
-      id: 58791,
-      known_for_department: 'Acting',
-      name: 'Patrick Fabian',
-      profile_path: '/ziTKeEraEksMQBg02adD22JJXb0.jpg',
-      character: 'Harvey Dent / Two-Face (voice)',
-    },
-  ],
-  crew: [
-    {
-      id: 10949,
-      known_for_department: 'Production',
-      name: 'Michael Uslan',
-      profile_path: '/cXiiH0SSk5UHCvHOVAhHX7tNuls.jpg',
-      job: 'Executive Producer',
-    },
-    {
-      id: 66266,
-      known_for_department: 'Writing',
-      name: 'Mike Mignola',
-      profile_path: '/cCYHC9bpVv8tfEzQ2dqlEEGOjX0.jpg',
-      job: 'Comic Book',
-    },
-  ],
-};
-
 const tabs = ['Cast', 'Crew'];
 
 export function CastAndCrew() {
