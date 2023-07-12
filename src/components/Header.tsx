634500eb425ac707eb1f5430cf71cb35b30cf03a
adetuadetu <jposorio95@hotmail.com>
2023-07-12 14:22:09 +0200

    Tobi's solution for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,22 @@
-import { HiArrowCircleLeft } from "react-icons/hi";
-import { useNavigate } from "react-router-dom"
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 type HeaderProps = {
-    children: string;
-}
+  children: string;
+};
 
 export default function HeaderFunction(calledProps: HeaderProps) {
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
-
-    return ( 
-        <header className=" ml items-center px-5 pb-6 pt-8 sticky top-0 z-10 bg-dark text-white m">
-            <div className="position: relative items-center">
-                    <button className="position: absolute " onClick={() => navigation(-1)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                        </svg>
-                    </button>
-                    <h1 className="flex justify-center typography-title" >{calledProps.children}</h1>
-            </div>
-        </header>
-    );
+  return (
+    <header className="flex items-center justify-center px-5 pb-6 pt-8 sticky top-0 z-10 bg-dark text-white">
+      <button
+        className="flex items-center absolute left-4 sm:left-8"
+        onClick={() => navigation(-1)}
+      >
+        <IoIosArrowBack className="w-6 h-6" />
+      </button>
+      <h1 className="text-center typography-title">{calledProps.children}</h1>
+    </header>
+  );
 }
