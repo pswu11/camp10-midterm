daee5b670984bcf1dac8a486474f80a4f9c95ff4
Stread <irgendwasmitmedien1@gmail.com>
2023-07-12 16:20:28 +0200

    PR review topics fixed

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -24,13 +24,15 @@ export function MovieDetails() {
   const { crew } = resCreditsData;
 
   const filteredCrew: string[] = [];
-  crew.map(x => {
+  crew.map((x, y) => {
     if (
       x.job === 'Director' ||
-      x.job == 'Novel' ||
-      (!x.job && x.known_for_department == 'Writing')
+      x.job === 'Novel' ||
+      (x.job !== 'Novel' && x.known_for_department == 'Writing')
     ) {
-      filteredCrew.push(x.name);
+      filteredCrew.push(
+        x.name.length >= 15 ? x.name.slice(0, 14) + '..' : x.name
+      );
     }
   });
 
@@ -60,7 +62,9 @@ export function MovieDetails() {
         src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
         alt=""
       />
-      <h1 className="text-white pt-6 pb-3 font-700 text-[1.25rem]">{title}</h1>
+      <h1 className="text-white pt-6 pb-3 font-700 text-[1.25rem]">
+        {title.length >= 30 ? title.slice(0, 31) + '..' : title}
+      </h1>
       <div className="flex pb-3 justify-between">
         <div className="flex gap-6">
           <p className="text-s text-white font-500">
@@ -82,17 +86,17 @@ export function MovieDetails() {
         </p>
       </div>
       <div className="flex justify-between items-center pb-4">
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 truncate">
           <p className="text-s text-white-dimmed font-700">
             Director:{' '}
-            <span className="text-white font-700 pl-[0.62rem]">
-              {filteredCrew[0] ?? ' '}
+            <span className="text-white font-700 pl-[0.62rem] ">
+              {filteredCrew[0] ?? ' Unknown '}
             </span>
           </p>
-          <p className="text-s text-white-dimmed font-500">
+          <p className="text-s text-white-dimmed font-500 overflow-hidden">
             Writer:{' '}
-            <span className="text-white font-500 pl-[1.44rem]">
-              {filteredCrew[1] ?? ' '}
+            <span className="text-white font-500 pl-[1.44rem] ">
+              {filteredCrew[1] ?? filteredCrew[3] ?? ' Unknown '}
             </span>
           </p>
         </div>
