de8ec44d29f715545171ca8889c4f33118a3ad1b
adetuadetu <jposorio95@hotmail.com>
2023-07-12 11:50:54 +0200

    centered text in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,15 @@ export default function HeaderFunction(calledProps: HeaderProps) {
     const navigation = useNavigate();
 
     return ( 
-        <header className="flex justify-between ml items-center px-5 pb-6 pt-8 sticky top-0 z-10 bg-dark text-white m">
-                <button onClick={() => navigation(-1)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                    </svg>
-                </button>
-                <h1 className="typography-title" >{calledProps.children}</h1>
+        <header className=" ml items-center px-5 pb-6 pt-8 sticky top-0 z-10 bg-dark text-white m">
+            <div className="position: relative items-center">
+                    <button className="position: absolute " onClick={() => navigation(-1)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                        </svg>
+                    </button>
+                    <h1 className="flex justify-center typography-title" >{calledProps.children}</h1>
+            </div>
         </header>
     );
 }
