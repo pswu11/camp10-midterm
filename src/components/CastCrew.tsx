f41645f4bdaf91a4373cde092e5b71ac0be042af
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-12 12:33:50 +0200

    fixed card: one person with multiple jobs

--- src/components/CastCrew.tsx
+++ src/components/CastCrew.tsx
@@ -1,12 +1,13 @@
-import { Cast, Crew } from '../types/api.ts';
+import { Cast, Crew } from '../types/api';
 
-type CastCrewProps = { person: Cast | Crew };
+type CastCrewProps = { person: Cast | (Crew & { jobs: string[] }) };
 
 export default function CastCrew({ person }: CastCrewProps) {
-  const isActor = (person: Cast | Crew): person is Cast =>
-    'character' in person;
+  const isActor = (
+    person: Cast | (Crew & { jobs: string[] })
+  ): person is Cast => 'character' in person;
   const { name: personName, profile_path: image } = person;
-  const role = isActor(person) ? person.character : person.job;
+  const roles = isActor(person) ? [person.character] : person.jobs;
   const imageUrl = image
     ? `https://image.tmdb.org/t/p/w500${image}`
     : 'https://1fid.com/wp-content/uploads/2022/06/no-profile-picture-9-1022x1024.jpg';
@@ -21,7 +22,7 @@ export default function CastCrew({ person }: CastCrewProps) {
       </div>
       <div className="flex flex-col justify-center">
         <h2 className=" text-white font-700 text-m">{personName}</h2>
-        <h4 className="text-s text-white-dimmed">{role}</h4>
+        <h4 className="text-s text-white-dimmed">{roles.join(', ')}</h4>
       </div>
     </div>
   );
--- src/pages/CastAndCrew.tsx
+++ src/pages/CastAndCrew.tsx
@@ -10,7 +10,17 @@ import { useLoaderData } from 'react-router-dom';
 const tabs = ['Cast', 'Crew'];
 
 export function CastAndCrew() {
-  const credits = useLoaderData() as Credits
+  const credits = useLoaderData() as Credits;
+
+  const groupedCrew = credits.crew.reduce((acc, person) => {
+    const existingPerson = acc.find(p => p.id === person.id);
+    if (existingPerson) {
+      existingPerson.jobs.push(person.job);
+    } else {
+      acc.push({ ...person, jobs: [person.job] });
+    }
+    return acc;
+  }, [] as (Crew & { jobs: string[] })[]);
 
   return (
     <div className="h-full w-full px-5 py-8 flex flex-col">
@@ -22,8 +32,8 @@ export function CastAndCrew() {
       </div>
       <Tab.Group>
         <Tab.List className="flex justify-between mt-8 mb-6">
-          {tabs.map(tab => (
-            <Tab as={Fragment}>
+          {tabs.map((tab, index) => (
+            <Tab key={index} as={Fragment}>
               {({ selected }) => (
                 <Button
                   variant="secondary"
@@ -40,15 +50,15 @@ export function CastAndCrew() {
             </Tab>
           ))}
         </Tab.List>
-        <Tab.Panels className="overflow-y-scroll">
-          <Tab.Panel className='space-y-4 container-snap snap-mandatory overflow-y-scroll'>
-            {credits.cast.map((person: Cast) => (
-              <CastCrew person={person} key={person.id} />
+        <Tab.Panels className="overflow-y-scroll  container-snap">
+          <Tab.Panel className="space-y-4">
+            {credits.cast.map((person: Cast, index) => (
+              <CastCrew person={person} key={index} />
             ))}
           </Tab.Panel>
-          <Tab.Panel className='space-y-4'>
-            {credits.crew.map((person: Crew) => (
-              <CastCrew key={person.id} person={person} />
+          <Tab.Panel className="space-y-4">
+            {groupedCrew.map((person, index) => (
+              <CastCrew key={index} person={person} />
             ))}
           </Tab.Panel>
         </Tab.Panels>
