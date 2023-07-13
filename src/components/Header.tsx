93b7e0b9b5e7cc5233db3eed99ae6879cf58f0ba
adetuadetu <jposorio95@hotmail.com>
2023-07-13 17:00:12 +0200

    second iteration of solution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,13 @@ type HeaderProps = {
   children: string;
 };
 
-export default function HeaderFunction(calledProps: HeaderProps) {
+export default function Header(title: HeaderProps) {
   const navigation = useNavigate();
 
   return (
-    <header className="flex items-center justify-center px-5 pb-6 pt-8 sticky top-0 z-10 bg-dark text-white">
-      <button
-        className="flex items-center absolute left-4 sm:left-8"
-        onClick={() => navigation(-1)}
-      >
-        <IoIosArrowBack className="w-6 h-6" />
-      </button>
-      <h1 className="text-center typography-title">{calledProps.children}</h1>
+    <header className="grid grid-cols-3 text-white">
+      <IoIosArrowBack className="ml-2 w-6 h-6" onClick={() => navigation(-1)}/>
+      <h1 className="text-center font-700 leading-5">{title.children}</h1>
     </header>
   );
 }
