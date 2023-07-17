85c2cf5546d0f637f6b1fa8a3cde29915d993e78
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-07-17 12:28:39 +0200

    tailwind styling

--- src/pages/SelectSeat.tsx
+++ src/pages/SelectSeat.tsx
@@ -8,6 +8,7 @@ import { useRouteLoaderData } from 'react-router-dom';
 import { Movie } from '../types/api';
 import { useTicketStore } from '../stores/ticket';
 import { useSeatLayoutStore } from '../stores/seats';
+import Header from '../components/Header';
 
 const seatPrices = {
   Front: 12.99,
@@ -72,37 +73,50 @@ export function SelectSeat() {
   const summaries = createBookingSummary(selectedSeats);
 
   return (
-    <>
-      <h4 className="text-white text-s">
-        This will later be filled by the header component
-      </h4>
-      <div className="flex-col px-7 justify-center items-center mx-7">
-        <div className=" bg-yellow h-1 w-63"></div>
-        <div className="h-5 opacity-25 bg-gradient-to-b from-yellow to-dark"></div>
-      </div>
-
-      <div className="grid grid-rows-6 grid-cols-9 gap-3 m-5">
-        {seatLayout.map((row, rowIndex) =>
-          row.map((seat, seatIdx) =>
-            seat.code ? (
-              <Seat
-                key={seatIdx}
-                seatCode={seat.code}
-                isSelected={seat.isSelected}
-                isReserved={seat.isReserved}
-                onClick={() => handleSeatClick(seat, rowIndex, seatIdx)}
-              />
-            ) : (
-              <div key={seatIdx} />
+    <div className="flex flex-col pt-8 h-full justify-between">
+      <div className="flex flex-col px-5 gap-y-8">
+        <Header title={'Select Seats'} />
+        <div className="flex flex-col w-2/3 self-center">
+          <div className=" bg-yellow h-1 w-full"></div>
+          <div className="h-5 opacity-25 bg-gradient-to-b from-yellow to-dark"></div>
+        </div>
+        <div className="grid grid-rows-6 grid-cols-9 gap-3">
+          {seatLayout.map((row, rowIndex) =>
+            row.map((seat, seatIdx) =>
+              seat.code ? (
+                <Seat
+                  key={seatIdx}
+                  seatCode={seat.code}
+                  isSelected={seat.isSelected}
+                  isReserved={seat.isReserved}
+                  onClick={() => handleSeatClick(seat, rowIndex, seatIdx)}
+                />
+              ) : (
+                <div key={seatIdx} />
+              )
             )
-          )
-        )}
+          )}
+        </div>
+        <div className="flex flex-row justify-center gap-6">
+          <div className="flex gap-2">
+            <div className="bg-dark-light w-4 h-4 rounded-full"></div>
+            <span className="text-white-dimmed text-s">Available</span>
+          </div>
+          <div className="flex justify-start gap-2">
+            <div className="bg-yellow w-4 h-4 rounded-full"></div>
+            <span className="text-white-dimmed text-s">Selected</span>
+          </div>
+          <div className="flex justify-start gap-2">
+            <div className="bg-white w-4 h-4 rounded-full "></div>
+            <span className="text-white-dimmed text-s">Reserved</span>
+          </div>
+        </div>
       </div>
       <BookingSummary
         summaries={summaries}
         buttonLink={`/movies/${currentMovie.id}/ticket`}
         buttonDisabled={seat.length === 0}
       />
-    </>
+    </div>
   );
 }
