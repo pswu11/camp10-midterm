401db73b4aa7e7477c3826e487baeffafb6c67e5
adetuadetu <jposorio95@hotmail.com>
2023-07-17 11:32:18 +0200

    Movie-Slider Hover bug, fix

--- src/components/MovieSlider.tsx
+++ src/components/MovieSlider.tsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 
 export function MovieSlider({ movies }: { movies: UpcomingMovie[] }) {
   return (
-    <div className="flex flex-row gap-8 container-snap snap-x snap-mandatory overflow-scroll w-[375px] absolute -ms-5">
+    <div className="flex flex-row gap-8 container-snap snap-x snap-mandatory overflow-scroll w-[375px] absolute -ms-5 -mx-5 py-3">
       {movies.map(movie => (
         <div key={movie.id} className="snap-center">
           <MovieCard movie={movie} variant="upcoming" />
